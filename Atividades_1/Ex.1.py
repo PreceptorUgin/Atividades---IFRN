b3f1173f47df3a6695c0be7bc007412b7d1d55f9
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-04-10 17:03:28 -0300

    Nu.Atv

---
+++ Atividades_1/Ex.1.py
@@ -0,0 +1,21 @@
+'''
+    Fazer um programa que solicite um número e calcule o seu fatorial
+
+    ATENÇÃO: Lembre-se das restrições para se calcular o fatorial
+             de um número
+'''
+import sys
+
+num = int(input("Insira um número: "))
+
+if num < 0 :
+    sys.exit()
+
+count = num
+fat = 1
+
+while count > 0:
+    fat = fat * count
+    count -= 1
+
+print(f'O fatorial do número {num} é {fat}')
