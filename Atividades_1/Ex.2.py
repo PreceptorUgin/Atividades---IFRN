8d4c0ff4c986cb0a0ff128af906ba696a288dd60
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-04-14 14:23:04 -0300

    Cod.2

--- Atividades_1/Ex.2.py
+++ Atividades_1/Ex.2.py
@@ -5,3 +5,27 @@
 
     ATENÇÃO: Considere que a Sequência de Fibonacci irá iniciar em 1 
 '''
+import sys
+
+n = int(input('Digite o número de elementos da Sequência de Fibonacci: '))
+
+if n <= 0:
+    print("Por favor, insira um número positivo maior que zero.")
+    sys.exit()
+
+n_count = n
+val_fib = 0
+fib1, fib2 = 0, 1
+
+if n == 1:
+    val_fib = fib1
+elif n == 2:
+    val_fib = fib2
+
+while n_count > 2:
+    val_fib = fib1 + fib2
+    fib1 = fib2
+    fib2 = val_fib
+    n_count -= 1
+
+print(f'O {n}º da sequência de Fibonaci é {val_fib}')
