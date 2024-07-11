c796c4b0e214304954fa70c830f327f0c0a25793
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-07-11 17:51:44 -0300

    Update Ex.7.py

--- Atividades_1/Ex.7.py
+++ Atividades_1/Ex.7.py
@@ -3,3 +3,20 @@
 
     soma = 1/1 + 3/2 + 5/3 + 7/4 + ... + 99/50
 '''
+
+import sys
+
+num = int(input('Informe um número.'))
+
+if num <= 0:
+    print('Número invalido, insira um número maior que 0')
+    sys.exit()
+
+som = 0
+dividendo = 1
+
+for divisor in range(1, 51):
+    som += (dividendo/divisor)
+    dividendo += 2
+
+print(som)
