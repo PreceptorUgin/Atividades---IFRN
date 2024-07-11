225c944b804405143005e82f038d9694b5da7e5d
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-07-11 17:05:21 -0300

    Update Ex.2.py

--- Atividades_1/Ex.2.py
+++ Atividades_1/Ex.2.py
@@ -10,22 +10,12 @@ import sys
 num = int(input('Digite o número de elementos da Sequência de Fibonacci: '))
 
 if num <= 0:
-    print('Por favor, insira um número positivo maior que zero.')
+    print('Por favor, insira um número positivo (diferente de zero).')
     sys.exit()
 
-n_count = num
-val_fib = 0
-fib1, fib2 = 0, 1
+now = 1
+then = 0
 
-if num == 1:
-    val_fib = fib1
-elif num == 2:
-    val_fib = fib2
-
-while n_count > 2:
-    val_fib = fib1 + fib2
-    fib1 = fib2
-    fib2 = val_fib
-    n_count -= 1
-
-print(f'O {num}º número da sequência de Fibonaci é {val_fib}.')
+for i in range(num):
+    print (now, end=",")
+    then, now = now, now + then
