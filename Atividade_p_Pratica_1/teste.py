cb2d492037c1d0b77c59f4810e2aca3e33ea44b0
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-07-22 17:46:20 -0300

    Update teste.py

--- Atividade_p_Pratica_1/teste.py
+++ Atividade_p_Pratica_1/teste.py
@@ -1,3 +1,19 @@
-d1 = 173
-d1 = d1/11
-print(d1)
+import sys
+
+val = str(input('Insira um valor inteiro: '))
+
+if not val.isnumeric():
+    sys.exit('Insira apenas numeros')
+elif int(val) < 0:
+    sys.exit('Insira um valor positivo')
+
+val = int(val)
+bi = 0
+val_fin = ''
+
+while val != 0:
+    bi = val%2
+    val_fin = str(bi) + val_fin
+    val = val//2
+
+print(val_fin)
