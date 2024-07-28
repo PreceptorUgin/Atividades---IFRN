43599583ba73dfe67af980e7a8ba3079941ae201
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-07-28 19:47:05 -0300

    Create 1.Tempo_de_Decaimento.py

---
+++ Atividades_Avaliativas/Atividade_Avaliativa_2/1.Tempo_de_Decaimento.py
@@ -0,0 +1,23 @@
+import sys
+mas_ini = str(input('Insira o valor de massa inicial(em gramas): '))
+
+if not mas_ini.isnumeric():
+    sys.exit('Insira um numero valido.')
+
+mas_ini = int(mas_ini)
+mas_fin = mas_ini
+temp_sec = 0
+while mas_fin > 0.49:
+    mas_fin = mas_fin/2
+    temp_sec += 50
+
+temp_min = 0
+temp_hor = 0
+while temp_sec >= 60:
+    temp_sec -= 60
+    temp_min += 1
+    if temp_min >= 60:
+        temp_min -= 60
+        temp_hor += 1
+
+print(f'Massa inicial:{mas_ini}, massa final:{mas_fin}, tempo decorrido:{temp_hor}:{temp_min}:{temp_sec}')
