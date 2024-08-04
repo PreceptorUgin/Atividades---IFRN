b5784a1e895a3030b6c771e393d5b70c269a01c7
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-08-04 12:25:33 -0300

    Update 2.Maximo_divisor_comum.py

--- Atividades_Avaliativas/Atividade_Avaliativa_2/2.Maximo_divisor_comum.py
+++ Atividades_Avaliativas/Atividade_Avaliativa_2/2.Maximo_divisor_comum.py
@@ -1,15 +1,32 @@
 import sys
-num_1 = input('Insira o primeiro número: ')
-num_2 = input('Insira o segundo número: ')
 
+#Declaração de variaveis.
+num_1 = int(input('Insira o primeiro número: '))
+num_2 = int(input('Insira o segundo número: '))
+
+nu_1 = num_1
+nu_2 = num_2
 mdc = 0
+num_aux = 0
 
+#Checagem dos inputs.
 if not str(num_1).isnumeric() or not str(num_2).isnumeric():
     sys.exit('Insira numeros validos.')
-elif int(num_1) < 0 or int(num_2) < 0:
+elif num_1 < 0 or num_2 < 0:
     sys.exit('Insira números positivos.')
-elif num_1 == 0:
-    mdc = num_2
-elif num_2 == 0:
-    mdc = num_1
 
+#Algoritimo euclidiano.
+while True:
+    if num_1 == 0:
+        mdc = num_2
+        break
+    elif num_2 == 0:
+        mdc = num_1
+        break
+
+    num_aux = num_1 % num_2
+    num_1 = num_2
+    num_2 = num_aux
+
+print(50*'-')
+print(f'Máximo Divisor Comum(MDC) entre, {nu_1} é {nu_2}\nséra {mdc}')
