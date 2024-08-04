657adf4568a3458d8beb10c38bd8a63c5b3b5397
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-08-04 14:39:09 -0300

    Update 4.Mutliplos_menores_q.py

--- Atividades_Avaliativas/Atividade_Avaliativa_2/4.Mutliplos_menores_q.py
+++ Atividades_Avaliativas/Atividade_Avaliativa_2/4.Mutliplos_menores_q.py
@@ -1,18 +1,15 @@
 #Declaração de variavel.
-mult = 0
 som = 0
 aux_1 = 0 
-aux_2 = 0
+aux_2 = 1
 
-for i in range(1, 1000000):
-    som = 0
+for i in range(1, 1000001):
     if (i%2) == 0 or (i%5) == 0:
+        som = 0
+        aux_2 = i
         while aux_2 > 0:
-            aux_2 = i
             aux_1 = aux_2 % 10
             som += aux_1**5
             aux_2 = aux_2//10
-            print((aux_1),(aux_2),(som))
         if i == som:
-            print((i))
-
+            print(som)
