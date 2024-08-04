3770147eedf4ef54c3bf920d35b6c8d345eb45c4
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-08-04 15:05:11 -0300

    Update 5.Quantidade_d_dig.py

--- Atividades_Avaliativas/Atividade_Avaliativa_2/5.Quantidade_d_dig.py
+++ Atividades_Avaliativas/Atividade_Avaliativa_2/5.Quantidade_d_dig.py
@@ -1,1 +1,18 @@
+import sys
+#Declaração de variaveis.
 val = int(input('Informe um numéro positivo: '))
+val_aux = val
+cont = 0
+
+#verificador.
+if val < 0:
+    sys.exit('Insira um numero positivo.')
+
+#Contador
+while val_aux > 0:
+    cont += 1
+    val_aux //= 10
+
+#Saida
+print(50*'-')
+print(f'O número de digitos em {val}, é {cont}')
