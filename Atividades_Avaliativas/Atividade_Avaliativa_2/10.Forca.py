cf426ac02f182a20ffdeca9d043bcc8b5bb606fb
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-08-04 23:10:12 -0300

    Create 10.Forca.py

---
+++ Atividades_Avaliativas/Atividade_Avaliativa_2/10.Forca.py
@@ -0,0 +1,21 @@
+p_chave = 'CHAVE'
+entr = ''
+cont = 0
+
+while True:
+    entr = str(input(': '))
+    if entr == p_chave:
+        print(50*'-')
+        print('CERTA RESPOSTA!')
+        break
+    else:
+        for i in range(len(p_chave)):
+            if entr[i] == p_chave:
+                print(50*'-')
+                print(f'Letra certa {entr[i]} em {i}')
+
+    if cont == 6:
+        print('Tentativas esgotadas.')
+        break
+    else:
+        cont += 1
