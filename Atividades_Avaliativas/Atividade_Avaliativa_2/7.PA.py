f54e08196615afed58dc95dd844414dc7a5e18a7
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-08-05 09:45:29 -0300

    Update 7.PA.py

--- Atividades_Avaliativas/Atividade_Avaliativa_2/7.PA.py
+++ Atividades_Avaliativas/Atividade_Avaliativa_2/7.PA.py
@@ -1,5 +1,6 @@
 import sys
 
+#Declaração de variaveis.
 val_ini = int(input('Insira um número: '))
 r_pa = float(input('Insira a razão da PA: '))
 elem_pa = int(input('Insira o número de elementos dessa PA: '))
@@ -10,17 +11,22 @@ aux = val_ini
 som = val_ini
 clas = ''
 
+#Tratamento dos input's
 if elem_pa < 0:
-    sys.exit('O número de elementos deve ser positivo')
+    sys.exit('O número de elementos deve ser positivo.')
+elif no_pa < 0:
+     sys.exit('A posição não pode receber números negativos.')
 else:
      print(50*'-')
      print(val_ini)
 
+#Calculador
 for i in range(int(elem_pa)):
         aux += r_pa
         som += aux
         print(aux)
 
+#Teste da classificação.
 if r_pa == 0:
     clas = 'Constante.'
 elif r_pa > 0:
@@ -28,4 +34,5 @@ elif r_pa > 0:
 else:
     clas = 'Decrescente.'
 
+#Saida.
 print(f'A PA é {clas}\nA soma dos seus elementos é, {som}\nO {no_pa}ºN dessa PA é, {fin_no_pa}')
