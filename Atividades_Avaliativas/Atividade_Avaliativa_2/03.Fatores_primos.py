a7bd825c183f0a92fce59397acebcaf21fb47cd4
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-08-06 12:31:02 -0300

    Update 03.Fatores_primos.py

--- Atividades_Avaliativas/Atividade_Avaliativa_2/03.Fatores_primos.py
+++ Atividades_Avaliativas/Atividade_Avaliativa_2/03.Fatores_primos.py
@@ -1,3 +1,32 @@
-import sys
-num_f_p = int(input('Insira o numero de fatores primos desejados: '))
+num = int(input('Digite a quantidade de fatores primos: '))
+qnt = int(input('Digite o número de números consecutivos: '))
 
+cont = 0
+aux = 2
+temp = aux
+f_prim = 0
+
+while cont < qnt:
+    temp = aux
+    f_prim = 0
+
+    while (temp % 2) == 0:
+        f_prim += 1
+        temp //= 2
+    fator = 3
+
+    while (fator * fator) <= temp:
+        while (temp % fator) == 0:
+            f_prim += 1
+            temp //= fator
+        fator += 2
+
+    if temp > 1:  
+        f_prim += 1
+
+    if f_prim == num:
+        print(aux)
+    
+    aux += 1
+
+print(f'Os primeiros {qnt} números consecutivos com exatamente {num} fatores primos são: {cont}')
