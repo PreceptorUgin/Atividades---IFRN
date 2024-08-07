fd22fe3c8a15242c9ce317680ca1b0b5db95ffca
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-08-06 23:26:49 -0300

    Update 04.Mutliplos_menores_q.py

--- Atividades_Avaliativas/Atividade_Avaliativa_2/04.Mutliplos_menores_q.py
+++ Atividades_Avaliativas/Atividade_Avaliativa_2/04.Mutliplos_menores_q.py
@@ -3,6 +3,7 @@ som = 0
 aux_1 = 0 
 aux_2 = 1
 print(50*'-')
+
 #Inicio do codigo.
 for i in range(1, 1000001):
     #Verificador.
@@ -10,11 +11,13 @@ for i in range(1, 1000001):
         #Reset de variaveis.
         som = 0
         aux_2 = i
+
         #Confirmador.
         while aux_2 > 0:
             aux_1 = aux_2 % 10
             som += aux_1**5
             aux_2 = aux_2//10
+            
         #Teste/Saida.
         if i == som:
             print(som)
