ca1175f0abe92c003b44bca2fc8d8a3b5da20e0b
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-08-26 20:51:08 -0300

    Create 02_Lista_de_valores.py

---
+++ Atividades_Avaliativas/Atividade_Avaliativa_3/02_Lista_de_valores.py
@@ -0,0 +1,20 @@
+import sys
+
+lstSize = int(input('Insira o tamanho da lista: '))
+lstVal = []
+
+if lstSize < 1:
+    sys.exit('O tamanho da lista tem que ser maior que 0.')
+
+while True:
+    lstEntr = int(input('Insira os valores dessa lista: '))
+    if lstEntr == 0:
+        break
+    elif len(lstVal) >= lstSize:
+        del lstVal[lstSize-1]
+    
+    lstVal.append(lstEntr)
+    lstVal.sort()
+    print(lstVal)
+
+print(f'\nSua lista final é, {lstVal}')
