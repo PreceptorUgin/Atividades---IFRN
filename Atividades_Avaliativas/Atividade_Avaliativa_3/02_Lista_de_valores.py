ed8bd0ba4bbdefbffad0c9ebc035ed52c48373c2
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-08-26 20:52:26 -0300

    Update 02_Lista_de_valores.py

--- Atividades_Avaliativas/Atividade_Avaliativa_3/02_Lista_de_valores.py
+++ Atividades_Avaliativas/Atividade_Avaliativa_3/02_Lista_de_valores.py
@@ -7,7 +7,7 @@ if lstSize < 1:
     sys.exit('O tamanho da lista tem que ser maior que 0.')
 
 while True:
-    lstEntr = int(input('Insira os valores dessa lista: '))
+    lstEntr = int(input('Insira um valor para a lista: '))
     if lstEntr == 0:
         break
     elif len(lstVal) >= lstSize:
