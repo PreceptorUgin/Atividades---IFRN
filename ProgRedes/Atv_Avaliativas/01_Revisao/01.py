f5ee7f991023accb1ff0dc95dafa746f2c96353f
Júlio <162058635+PreceptorUgin@users.noreply.github.com>
2024-11-12 14:08:17 -0300

    Update 01.py

--- ProgRedes/Atv_Avaliativas/01_Revisao/01.py
+++ ProgRedes/Atv_Avaliativas/01_Revisao/01.py
@@ -1,30 +1,37 @@
 import sys
 
-lstMask = []
-try:
-    ipAddr = str(input("Insira o endereço de rede: "))
-    nMask = int(input("Insira o numero de mascaras que serao usadas no calculo: "))
-except Exception as e:
-    sys.exit(f"Erro {e}")
-    raise
-
-lstOct = ipAddr.split('.')
-if len(lstOct) != 4:
-    sys.exit('Endereço IP invalido.')
-
-lstOct = [int(i) for i in lstOct]
-
-for n in range(1, nMask+1):
+def obterEndereco():
     try:
-        Masks = int(input(f"Digite a {n}º mascara(CIDR): "))
+        ipAddr = str(input("Insira o endereço de rede: "))
+        lstOct = ipAddr.split('.')
+
+        if len(lstOct) != 4 or not all(lstOct.isdigit() and 0 <= int(octeto) <= 255 for octeto in lstOct):
+            sys.exit('Endereço IP invalido.')
+        
+        return [int(octeto) for octeto in lstOct]
+    
     except Exception as e:
-        sys.exit(f"Erro {e}")
-        raise
+        sys.exit(f"Erro ao inserir o endereço IP: {e}")
 
-    if Masks >= 32 or Masks <= 0:
-        n -= 1
-        print('Valor inserido invalido, insira valores entre 1 e 31')
-    else:
-        lstMask.append(Masks)
+def obterMascaras(mask_count):
+    lstMask = []
+    for n in range(1 ,mask_count):
+        try:
+            mask = int(input(f"Digite a {n}º máscara (CIDR entre 1 e 31): "))
+            if 0 >= Masks >= 32:
+                n -= 1
+                print('Valor inserido invalido, insira valores entre 1 e 31')
+            else:
+                print('Valor inválido. Insira uma máscara CIDR entre 1 e 31.')
+        except ValueError:
+            lstMask.append(mask)
+    return lstMask
 
-print(lstMask, lstOct)
+# Programa Principal
+try:
+    ip_addr = obterEndereco()
+    nMask = int(input("Insira o número de máscaras que serão usadas no cálculo: "))
+    lst_mask = obterMascaras(nMask+1)
+
+except Exception as e:
+    sys.exit(f"Ocorreu um erro: {e}")
